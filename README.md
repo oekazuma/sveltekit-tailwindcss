efda9848df349ec65af68dcbd80f37ba3656608b
oekazuma <koneko0604@gmail.com>
2022-04-26 19:49:06 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# SvelteKit-TailwindCSS
+
+## Technology stack
+
+uses the following technologies:
+
+- [Svelte](https://svelte.dev)
+- [SvelteKit](https://kit.svelte.dev)
+- [Tailwind CSS](https://tailwindcss.com/)
---
+++ postcss.config.cjs
@@ -0,0 +1,18 @@
+const tailwindcss = require('tailwindcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+
+const production = process.env.NODE_ENV === 'production';
+
+const config = {
+  plugins: [
+    tailwindcss(),
+    autoprefixer,
+    production &&
+    cssnano({
+      preset: 'default',
+    }),
+  ],
+};
+
+module.exports = config;
